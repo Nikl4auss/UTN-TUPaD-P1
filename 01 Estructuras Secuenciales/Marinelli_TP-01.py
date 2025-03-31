10a8410021f75ed782fa10b5d97ad495fbd51a29
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2025-03-31 08:23:01 -0300

    Añadido el primer trabajo practico

---
+++ 01 Estructuras Secuenciales/Marinelli_TP-01.py
@@ -0,0 +1,103 @@
+#Ejercicio 1
+
+# print("Hola mundo!")
+
+#Ejercicio 2
+
+# nombre = input("Ingrese su nombre: ")
+
+# print(f"Hola {nombre}!")
+
+#Ejercicio 3
+
+# nombre = input("Ingrese su nombre: ")
+# apellido = input("Ingrese su apellido: ")
+# edad = input("Ingrese su edad: ")
+# residencia = input("Ingrese su lugar de residencia: ")
+
+# print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.")
+
+#Ejercicio 4
+
+# radio = float(input("Ingrese el radio del círculo a calcular su area y perimetro: "))
+
+# area_circulo = 3.1416 * radio ** 2
+# perimetro_circulo = 2 * 3.1416 * radio
+
+# print(f"El área del círculo es: {area_circulo} y el perímetro es: {perimetro_circulo}")
+
+#Ejercicio 5
+
+# segundos = int(input("Ingrese la cantidad de segundos a convertir en horas: "))
+
+# horas = segundos // 3600
+
+# print(f"La cantidad de segundos ingresados equivalen a {horas} horas.")
+
+#Ejercicio 6
+
+# numero = int(input("Ingrese el numero del cual desea ver su tabla de multiplicación: "))
+
+# print(f"Tabla de multiplicar del {numero}")
+# print(f"{numero} x 1 = {numero * 1}")
+# print(f"{numero} x 2 = {numero * 2}")
+# print(f"{numero} x 3 = {numero * 3}")
+# print(f"{numero} x 4 = {numero * 4}")
+# print(f"{numero} x 5 = {numero * 5}")
+# print(f"{numero} x 6 = {numero * 6}")
+# print(f"{numero} x 7 = {numero * 7}")
+# print(f"{numero} x 8 = {numero * 8}")
+# print(f"{numero} x 9 = {numero * 9}")
+# print(f"{numero} x 10 = {numero * 10}")
+
+#Ejercicio 7
+
+# print("Ingrese dos números enteros distintos de 0")
+
+# numero1 = int(input("Ingrese el primer número: "))
+# numero2 = int(input("Ingrese el segundo número: "))
+
+# suma = numero1 + numero2
+# resta1 = numero1 - numero2
+# resta2 = numero2 - numero1
+# multiplicacion = numero1 * numero2
+# division1 = numero1 / numero2
+# division2 = numero2 / numero1
+
+# print(f"La suma de los números es: {suma}")
+# print(f"La resta de {numero1} - {numero2} es: {resta1}")
+# print(f"La resta de {numero2} - {numero1} es: {resta2}")
+# print(f"La multiplicación de los números es: {multiplicacion}")
+# print(f"La división de {numero1} / {numero2} es: {division1}")
+# print(f"La división de {numero2} / {numero1} es: {division2}")
+
+#Ejercicio 8
+
+# peso = float(input("Ingrese su peso en kg: "))
+# altura = float(input("Ingrese su altura en metros: "))
+
+# imc = peso / altura ** 2
+
+# print(f"Su índice de masa corporal es: {imc}")
+
+#Ejercicio 9
+
+# celcius = float(input("Ingrese la temperatura en grados Celcius: "))
+
+# fahrenheit = celcius * 9/5 + 32
+
+# print(f"La temperatura en grados Fahrenheit es: {fahrenheit}")
+
+#Ejercicio 10
+
+# print("Ingrese los 3 numeros a promediar: ")
+
+# num1 = int(input("Ingrese el primer número: "))
+# num2 = int(input("Ingrese el segundo número: "))
+# num3 = int(input("Ingrese el tercer número: "))
+
+# promedio = (num1 + num2 + num3) / 3
+
+# print(f"El promedio de los números ingresados es: {promedio}")
+
+print(10 + 3.0)
